56753462ca43658364fbbac4adeb4731eec3c184
chndnnn <chndn136@gmail.com>
2025-02-14 22:18:56 +0530

    profile screen changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import MainContextProvider from "../context/mainContext";
 import Home from "./Screens/Home";
+import ProfileScreen from "./Screens/profileScreen";
 
 function App() {
   return (
     <MainContextProvider>
       <div className="min-h-screen bg-gradient-to-b from-[#050008] to-[#00456E]">
-        <Home />
+        {/* <Home /> */}
+        <ProfileScreen />
       </div>
     </MainContextProvider>
   );
